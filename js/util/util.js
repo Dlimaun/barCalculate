8a70579304b5bb3111638d5adb2a7903e049e864
Bruno <Bruno@Inspiron>
2013-10-18 11:08:51 -0300

    Transition

--- js/util/util.js
+++ js/util/util.js
@@ -1,32 +1,39 @@
 function soNumeros(evt) {
     var theEvent = evt || window.event;
-    var key = ( theEvent.which ) ? theEvent.which : theEvent.keyCode
-    key = String.fromCharCode( key );
+    var key = (theEvent.which) ? theEvent.which : theEvent.keyCode
+    key = String.fromCharCode(key);
     var regex = /[0-9]/;
-    if ([evt.keyCode||evt.which]==8 || [evt.keyCode||evt.which]== 9 || [evt.keyCode||evt.which]== 13) //this is to allow backspace (8), tab(9), enter(13)
+    if ([evt.keyCode || evt.which] == 8 || [evt.keyCode || evt.which] == 9 || [evt.keyCode || evt.which] == 13) //this is to allow backspace (8), tab(9), enter(13)
         return true;
-    if( !regex.test(key) ) {
+    if (!regex.test(key)) {
         theEvent.returnValue = false;
         theEvent.preventDefault();
     }
 }
 
-jQuery(".link").off().on('click', function(){
+jQuery(".link").off().on('click', function() {
     jQuery('.link').removeClass('active');
     jQuery(this).addClass('active');
-    
+
     var li_link = jQuery(this),
-    href        = li_link.data('href');
-                    
-    set_pagina(href);    
+            href = li_link.data('href');
+
+    set_pagina(href);
 });
 
-var pageHref;;
+var pageHref;
+;
 function set_pagina(href) {
     pageHref = href;
     console.log('clicked page', href);
-    jQuery('#page').children().remove();
-    jQuery('#page').load(href);
+    var div = jQuery(document.createElement('div'));
+    div.load(href);
+    div.addClass("container");
+    div.addClass("fade-in");
+    var page = jQuery('#page');
+    page.children().remove();
+    page.append(div);
+
 }
 
 
